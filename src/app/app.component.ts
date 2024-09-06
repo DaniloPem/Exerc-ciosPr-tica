2bdcffa1363519aeea8b62ea1b24f1e286ebed0d
Danilo Pezo <danilo_97pem@hotmail.com>
2024-09-05 23:50:55 -0300

    fibonacci

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'fibonacci';
+  numeroInserido!: number;
+  result!: string;
+
+  acharNumero() {
+    this.result = '';
+    if (this.numeroInserido < 0) {
+      this.result = 'O número informado não pertence a sequência de Fibonacci';
+    } else if (this.numeroInserido === 0 || this.numeroInserido === 1) {
+      this.result = 'O número informado pertence a sequência de Fibonacci';
+    } else {
+      let proximoNumero = 0;
+      let fibonacciNumeros: Array<number> = [0, 1];
+      while (this.numeroInserido > proximoNumero) {
+        let fibonacciNumerosLength = fibonacciNumeros.length;
+        proximoNumero =
+          fibonacciNumeros[fibonacciNumerosLength - 1] +
+          fibonacciNumeros[fibonacciNumerosLength - 2];
+        fibonacciNumeros.push(proximoNumero);
+        this.result =
+          proximoNumero === this.numeroInserido
+            ? 'O número informado pertence a sequência de Fibonacci'
+            : 'O número informado não pertence a sequência de Fibonacci';
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
