1d276e97a87a3fa25a81f864af6338d8212e4a44
Danilo Pezo <danilo_97pem@hotmail.com>
2024-09-07 18:35:55 -0300

    Projeto temrinado

--- src/app/soma/soma.component.ts
+++ src/app/soma/soma.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SomaComponent {
   pergunta: string =
-    'Observe o mtrecho do código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça {K = K +1; SOMA = SOMA + K} IMPRIMIR(SOMA);';
+    'Observe o trecho do código abaixo: int INDICE = 12, SOMA = 0, K = 1; enquanto K < INDICE faça {K = K +1; SOMA = SOMA + K} IMPRIMIR(SOMA);';
   indice: number = 12;
   resposta: string = this.acharNovoValorDeSoma();
 
